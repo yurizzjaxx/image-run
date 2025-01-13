e271d7a2401305e1b6f6354458abb6119bc53ecc
yuriZzJaxx ZT <132253210+yurizzjaxx@users.noreply.github.com>
2025-01-12 21:40:33 -0400

    Add files via upload

---
+++ fire/apiKey/firebaseKey.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCe3_DqXAYCBHQjIWthbiawqe2J-4Sxb1M",
+  authDomain: "web-app-zt.firebaseapp.com",
+  databaseURL: "https://web-app-zt-default-rtdb.firebaseio.com",
+  projectId: "web-app-zt",
+  storageBucket: "web-app-zt.firebasestorage.app",
+  messagingSenderId: "96129972051",
+  appId: "1:96129972051:web:a182f0ac0525a151b693a6",
+  measurementId: "G-J99XC30SP4"
+};
+
